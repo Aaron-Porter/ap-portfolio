5081053151bbf1753773c63d2ad15c511ab9e008
Aaron Porter <aaronporter95@gmail.com>
2021-05-18 08:09:38 -0400

    Switch to stitches
    - Remove built-in css files
    - Added stitches library
    - Add SSR for stitches

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { getCssString } from '../stitches.config';
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    try {
+      const initialProps = await NextDocument.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
+          </>
+        ),
+      };
+    } finally {
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
