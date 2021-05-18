8afc58671d0d4f64019f1d2bb4e2268bd287dff8
Aaron Porter <aaronporter95@gmail.com>
2021-05-18 08:18:24 -0400

    Linting & DUX improvements
    - Add linting presets
    - Add import from base url

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error",
+    "anchor-is-valid": "off",
+    camelcase: "always",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        moduleDirectory: ["node_modules", "."],
+      },
+    },
+  },
+}
