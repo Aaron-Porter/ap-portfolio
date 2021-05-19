c89f2d3325fb7d742973f3d232b3dba4193ffd94
Aaron Porter <aaronporter95@gmail.com>
2021-05-19 09:15:59 -0400

    Add custom fonts

--- stitches.config.js
+++ stitches.config.js
@@ -18,9 +18,20 @@ export const { styled, getCssString, global } = createCss({
 });
 
 export const globalStyles = global({
+  "@font-face": [
+    {
+      fontFamily: "Suisse",
+      src: `url("fonts/Suisse/SuisseIntl-Regular-WebS.woff2") format("woff2"),
+            url("fonts/Suisse/SuisseIntl-Regular-WebS.woff") format("woff"),
+            url("fonts/Suisse/SuisseIntl-Regular-WebS.ttf")  format("truetype")`,
+      fontStyle: "normal",
+      fontWeight: 400,
+    },
+  ],
   body: {
     margin: 0,
     padding: 0,
+    fontFamily: "Suisse",
     "-webkit-font-smoothing": "antialiased",
     "-moz-osx-font-smoothing": "grayscale",
   },
