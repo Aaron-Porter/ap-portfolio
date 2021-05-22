ace536e714c31e3ae9750886f1f0af68a4ddd302
Aaron Porter <aaronporter95@gmail.com>
2021-05-21 20:23:06 -0400

    Add svg config for webpack

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   future: {
     webpack5: true,
   },
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
