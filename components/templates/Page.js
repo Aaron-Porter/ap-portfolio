54cc01beb9d56216d2bd339111e02789f2f44c61
Aaron Porter <aaronporter95@gmail.com>
2021-05-22 09:12:03 -0400

    Add header & footer

--- components/templates/Page.js
+++ components/templates/Page.js
@@ -3,20 +3,49 @@ import Flex from "components/atoms/Flex";
 import Text from "components/atoms/Text";
 import Content from "components/atoms/Content";
 
+const Header = () => (
+  <Box css={{ padding: "$space500 0" }}>
+    <Content>
+      <Flex>
+        <Box>
+          <Text preset="heading" css={{ color: "$accent" }}>
+            A.
+          </Text>
+        </Box>
+      </Flex>
+    </Content>
+  </Box>
+);
+
+const Footer = () => (
+  <Box css={{ padding: "$space700 0 $space500" }}>
+    <Content>
+      <Flex gap>
+        <Box>
+          <Text preset="overline" css={{ color: "$gray500" }}>
+            hello@aaronporter.co
+          </Text>
+        </Box>
+        <Box>
+          <Text preset="overline" css={{ color: "$gray500" }}>
+            Twitter
+          </Text>
+        </Box>
+        <Box>
+          <Text preset="overline" css={{ color: "$gray500" }}>
+            LinkedIn
+          </Text>
+        </Box>
+      </Flex>
+    </Content>
+  </Box>
+);
+
 const Page = ({ children }) => (
   <Box>
-    <Box css={{ padding: "$space500 0" }}>
-      <Content>
-        <Flex>
-          <Box>
-            <Text preset="heading" css={{ color: "$accent" }}>
-              A.
-            </Text>
-          </Box>
-        </Flex>
-      </Content>
-    </Box>
+    <Header />
     {children}
+    <Footer />
   </Box>
 );
 
