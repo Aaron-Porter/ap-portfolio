191f848fa52cb9dae5157054cde3230be227b749
Aaron Porter <aaronporter95@gmail.com>
2021-05-24 09:20:43 -0400

    Add project template

---
+++ components/templates/Project.js
@@ -0,0 +1,17 @@
+import Box from "components/atoms/Box";
+import { Header } from "../molecules/Header";
+import { Footer } from "../molecules/Footer";
+import Section from "../atoms/Section";
+import Content from "../atoms/Content";
+import Flex from "../atoms/Flex";
+import Text from "../atoms/Text";
+
+const Project = ({ children }) => (
+  <Box>
+    <Header />
+    {children}
+    <Footer />
+  </Box>
+);
+
+export default Project;
