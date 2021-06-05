7545b6f10c8b8d62613057b298f772bac27b6bd8
Aaron Porter <aaronporter95@gmail.com>
2021-06-05 09:56:42 -0400

    New menu

--- components/molecules/Header.js
+++ components/molecules/Header.js
@@ -5,21 +5,39 @@ import Content from "components/atoms/Content";
 import LinkTo from "components/utilities/LinkTo";
 import { useRouter } from "next/router";
 import { styled } from "stitches.config";
+import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import Image from "next/image";
+import React from "react";
+import Icon from "components/atoms/Icon";
+
+const _Menu = styled(Flex, {
+  backgroundColor: "white",
+  padding: "$space100",
+  borderRadius: "999px",
+  flexDirection: "row",
+  overflow: "hidden",
+});
+
+const Avatar = styled(Box, {
+  backgroundImage: "url('images/avatar.png')",
+});
 
 const Item = styled(Box, {
   transition: "$default",
+  padding: "$space200 $space400",
+  position: "relative",
 
   "&:hover": {
-    color: "$gray000",
+    color: "$accent",
   },
 
   variants: {
     active: {
       true: {
-        color: "$gray000",
+        color: "rgba(219, 66, 34, 1)",
       },
       false: {
-        color: "$gray400",
+        color: "$gray600",
       },
     },
   },
@@ -29,37 +47,121 @@ const Item = styled(Box, {
   },
 });
 
+const Highlight = styled(Box, {
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: "rgba(255, 233, 228, 1)",
+  borderRadius: "999px",
+  zIndex: 0,
+  display: "block",
+});
+
+const Menu = ({ children }) => <_Menu layout>{children}</_Menu>;
+
+const MenuItem = ({ href = "", path, label, children }) => {
+  const active = path === href;
+
+  return (
+    <LinkTo href={href} displayContents>
+      <Item
+        active={active}
+        css=""
+        key={label}
+        layout
+        layoutId={label}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <Flex>
+          {children}
+          <Text preset="body" css={{ color: "inherit", zIndex: 10 }}>
+            {label}
+          </Text>
+        </Flex>
+        {active && (
+          <Highlight
+            layout
+            layoutId="highlight"
+            key="highlight"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          />
+        )}
+      </Item>
+    </LinkTo>
+  );
+};
+
 export const Header = () => {
   const router = useRouter();
+  const path = router.pathname;
 
-  const MenuItem = ({ children, href = "" }) => {
-    const active = router.pathname === href;
-    return (
-      <Item active={active}>
-        <LinkTo href={href}>
-          <Text preset="body" css={{ color: "inherit" }}>
-            {children}
-          </Text>
-        </LinkTo>
-      </Item>
-    );
-  };
+  const isProject = path.includes("project");
+  React.useEffect(() => {
+    console.log("mount");
+  }, []);
 
   return (
     <Box css={{ padding: "$space500 0" }}>
       <Content>
-        <Flex justify="between" align="end">
-          <Box>
-            <Text preset="heading" css={{ color: "$accent", paddingBottom: 0 }}>
-              A.
-            </Text>
-          </Box>
-          <Box>
-            <Flex gap>
-              <MenuItem href="/">Work</MenuItem>
-              <MenuItem href="/about">About Me</MenuItem>
-            </Flex>
-          </Box>
+        <Flex>
+          <AnimateSharedLayout>
+            <AnimatePresence>
+              {!isProject && (
+                <Box
+                  css={{
+                    borderRadius: "50%",
+                    marginRight: "$space200",
+                    overflow: "hidden",
+                    width: "64px",
+                    height: "64px",
+                    position: "relative",
+                  }}
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  exit={{ scale: 0 }}
+                  layout
+                  key="avatar"
+                  layoutId="avatar"
+                >
+                  <Image src="/images/avatar.png" layout="fill" />
+                </Box>
+              )}
+              <Menu key="menu">
+                {isProject && (
+                  <MenuItem
+                    href="/"
+                    path={path}
+                    label="All Work"
+                    key="all-work"
+                  >
+                    <Icon
+                      name="ArrowsClockwise"
+                      css={{
+                        width: "24px",
+                        height: "24px",
+                        marginRight: "$space200",
+                      }}
+                    />
+                  </MenuItem>
+                )}
+                {!isProject && (
+                  <MenuItem href="/" path={path} label="Work" key="work" />
+                )}
+                <MenuItem
+                  href="/about"
+                  path={path}
+                  label="About Me"
+                  key="about"
+                />
+              </Menu>
+            </AnimatePresence>
+          </AnimateSharedLayout>
         </Flex>
       </Content>
     </Box>
