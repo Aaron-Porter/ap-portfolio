ee924553d3c3559154f22d694bf2212d4714ce91
Aaron Porter <aaronporter95@gmail.com>
2021-06-21 09:19:03 -0400

    Add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -60,6 +60,52 @@ export default class Document extends NextDocument {
             type="font/woff2"
             crossOrigin="anonymous"
           />
+
+          {/* Generics */}
+          <link rel="icon" href="/favicons/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" href="/favicons/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" href="/favicons/favicon-96x96.png" sizes="96x96" />
+          <link
+            rel="icon"
+            href="/favicons/favicon-128x128.png"
+            sizes="128x128"
+          />
+          <link
+            rel="icon"
+            href="/favicons/favicon-196x196.png"
+            sizes="192x192"
+          />
+
+          {/* Android */}
+          <link
+            rel="shortcut icon"
+            sizes="196x196"
+            href="/favicons/favicon-196x196.png"
+          />
+
+          {/* iOS */}
+          <link
+            rel="apple-touch-icon"
+            href="/favicons/apple-touch-icon-120x120.png"
+            sizes="120x120"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/favicons/apple-touch-icon-152x152.png"
+            sizes="152x152"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/favicons/apple-touch-icon-180x180.png"
+            sizes="180x180"
+          />
+
+          {/* Windows */}
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
+          <meta
+            name="msapplication-TileImage"
+            content="/favicons/mstile-144x144.png"
+          />
         </Head>
         <body>
           <Main />
